08fea8c77923dc670eca4db4f07f7ecfde81a2a5
Novikov Maxim <maxon.nowik@yandex.ru>
2025-11-01 17:07:57 +0100

    add yamdb_with_workflow

---
+++ yamdb_with_CICD_and_docker/api_yamdb/user/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework import routers
+from user.views import UsersViewSet, sign_up, token_obtain
+
+v1_router = routers.DefaultRouter()
+v1_router.register('users', UsersViewSet, basename='users')
+
+urlpatterns = [
+    path('', include(v1_router.urls)),
+    path('auth/signup/', sign_up, name='sign_up'),
+    path('auth/token/', token_obtain, name='token_obtain_pair')
+]
